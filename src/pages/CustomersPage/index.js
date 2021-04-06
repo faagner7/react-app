d2f57250b4fac6f487e09f7606e9689e31b09a46
Gabriel Motta <gabrielmottagc@gmail.com>
2021-04-06 15:37:40 -0300

    added api helper

--- src/pages/CustomersPage/index.js
+++ src/pages/CustomersPage/index.js
@@ -1,6 +1,9 @@
 import React from "react";
+import api from "../../helpers/api";
 
 function CustomersPage() {
+  const { url } = api;
+  console.log(url);
   return (
     <>
       <h1>CustomersPage</h1>
