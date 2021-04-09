2b397fec3d1b407df32e30e8c02fa1988c7b73ca
Fagner Rodrigues <faagnerrodrigues@gmail.com>
2021-04-09 15:37:19 -0300

    added example

--- src/pages/CustomersPage/index.js
+++ src/pages/CustomersPage/index.js
@@ -1,58 +1,23 @@
-import React from "react";
+import React, { useEffect } from "react";
 
-import Table from "@material-ui/core/Table";
-import TableBody from "@material-ui/core/TableBody";
-import TableCell from "@material-ui/core/TableCell";
-import TableHead from "@material-ui/core/TableHead";
-import TableRow from "@material-ui/core/TableRow";
+//COMPONENTE
+const CustomersPage = () => {
+  //FUNCAO PEGAR DADOS API
+  const getCustomersRequest = () => {
+    // AQUI VAI A REQUEST.
+    console.log("disparou a função!");
+  };
 
-const arrayRows = [
-  {
-    id: 1,
-    name: "Andrew",
-    cpf: "123.345.135-92",
-    address: "Rua Anchieta, 1020",
-    country: "Brazil",
-  },
-  {
-    id: 2,
-    name: "Julio",
-    cpf: "123.345.135-23",
-    address: "Rua Padre Chagas, 120",
-    country: "Canadá",
-  },
-  {
-    id: 3,
-    name: "Lorrane",
-    cpf: "123.345.135-21",
-    address: "Rua Padre Chagas, 10",
-    country: "Canadá",
-  },
-];
+  //AO MONTAR PÁGINA
+  useEffect(() => {
+    getCustomersRequest();
+  }, []);
 
-export default function CustomersPage() {
   return (
-    <Table>
-      <TableHead>
-        <TableRow>
-          <TableCell>ID do Cliente</TableCell>
-          <TableCell align="right">Name</TableCell>
-          <TableCell align="right">CPF</TableCell>
-          <TableCell align="right">Addres</TableCell>
-          <TableCell align="right">Country</TableCell>
-        </TableRow>
-      </TableHead>
-      <TableBody>
-        {arrayRows.map((item) => (
-          <TableRow key={item.name}>
-            <TableCell>{item.id}</TableCell>
-            <TableCell>{item.name}</TableCell>
-            <TableCell>{item.cpf}</TableCell>
-            <TableCell>{item.address}</TableCell>
-            <TableCell>{item.country}</TableCell>
-          </TableRow>
-        ))}
-      </TableBody>
-    </Table>
+    <div>
+      <p>CustomersPage</p>
+    </div>
   );
-}
+};
+
+export default CustomersPage;
